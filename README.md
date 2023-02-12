6b6888011fddc5481af4e6219d6d6bcf52edd55c
antonvw <a.m.vanwezenbeek@gmail.com>
2023-02-12 10:00:55 +0100

    updated README and script

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# admin
+# NHPO admin
+
+This repo contains some scripts to facilitate admin tasks for the treasurer of
+the orkest.
+
+## usage
+
+Currently the `merge-ingb*` are the only scripts, can be used to
+create a csv overview from the ingb bank account that can
+be imported into a spreadsheet for the annual meeting presentation or
+municipal subsidy.
+
+## example
+
+```bash
+./admin/merge-ingb.sh NL89INGB0003788111_01-01-2022_31-12-2022.csv
+```
--- merge-ingb.sh
+++ merge-ingb.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [[ $1 == "-h" ]]
-  then
-    echo "use merge-ingb.sh -h [csv-file]"
-    exit 1
+if [[ $1 == "-h" ]] || [[ $# -ne 1 ]]
+then
+  echo "use: merge-ingb.sh csv-file"
+  exit 1
 fi
 
-input=overzicht-2022.csv
-output=overzicht-2022.ods
+input=$1
 
-if [[ $# -eq 1 ]]
-  then
-    input=$1
-    output=$1-nhpo.csv
+if [[ ! -f $1 ]]
+then
+  echo "file $1 does not exist"
+  exit 1
 fi
+
+output=$1-nhpo.csv
 
 awk -f admin/merge-ingb.awk $input > $output
